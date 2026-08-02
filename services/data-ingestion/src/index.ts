7cbd5c23ebb76aaac6a5ad61400f6bb5534ad377
micheal11844-web <micheal11844@gmail.com>
2026-08-02 01:53:40 +0100

    Update index.ts

--- services/data-ingestion/src/index.ts
+++ services/data-ingestion/src/index.ts
@@ -3,3 +3,9 @@ export type {
   IngestionResult,
   IngestionTrigger,
 } from "./DataIngestionConnector.js";
+export {
+  NasaPowerConnector,
+  parsePowerResponse,
+  type NasaPowerLocation,
+  type NasaPowerConnectorConfig,
+} from "./connectors/nasa-power-connector.js";
